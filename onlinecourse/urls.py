7c8ed0bd4979643915b51374f97508f98e1a76a1
simrah678 <simrahshamil357@gmail.com>
2026-02-26 14:33:32 -0500

    Added submit and result urls

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -4,21 +4,30 @@ from django.conf.urls.static import static
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-    path(route='', view=views.CourseListView.as_view(), name='index'),
+    # Home page
+    path('', views.CourseListView.as_view(), name='index'),
+
+    # Authentication
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
-    # ex: /onlinecourse/5/
+
+    # Course details
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
-    # ex: /enroll/5/
+
+    # Enrollment
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
-    # <HINT> Create a route for submit view
+    # ✅ Submit exam
+    path('submit/<int:course_id>/',
+         views.submit,
+         name='submit'),
 
-    # <HINT> Create a route for show_exam_result view
+    # ✅ Show exam result
+    path('exam_result/<int:course_id>/<int:submission_id>/',
+         views.show_exam_result,
+         name='show_exam_result'),
 
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
